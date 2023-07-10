0b2355a1f8cc5341850821d6d2998076cb06d8c5
Ignacio Montero <ig_montero@hotmail.com>
2023-07-10 19:17:45 -0300

    first commit. coming soon page

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import ComingSoon from './pages/ComingSoon';
+import './assets/scss/themes.scss';
+import './assets/scss/icons.scss';
+import 'remixicon/fonts/remixicon.css'
+
+function App() {
+	return (
+		<>
+			<ComingSoon />
+		</>
+	);
+}
+
+export default App;
